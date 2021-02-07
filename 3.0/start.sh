c5d6a0b02313435c732fb6082602bf1b7a657ac0
JonasBonno <14278040+JonasBonno@users.noreply.github.com>
2021-02-07 14:36:28 +0100

    Update start.sh

--- 3.0/start.sh
+++ 3.0/start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Install updates as root user
-apk update && rm -rf /var/lib/apt/lists/*
-pip3 install --upgrade pip && rm -rf ~/.cache/pip/*
+# Install updates
+apk update
+pip3 install --upgrade pip Red-DiscordBot
 
-# Install updates as redbot user
-#su -s /bin/bash -c "pip3 install --upgrade pip Red-DiscordBot" -m "redbot"
-#su -s /bin/bash -c "rm -rf ~/.cache/pip/*" -m "redbot"
-
-# Start server as redbot user
-su -s /bin/bash -c "~/.local/bin/redbot mybot" -m "redbot"
+# Start server
+redbot mybot
